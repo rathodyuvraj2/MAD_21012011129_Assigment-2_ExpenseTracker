02f0f6427b15ddb08934584d05dae83b220e082e
Yuvraj <rathodyuvrajsinh2004@gmail.com>
2023-10-27 22:53:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_21012011129_assigment_2_expensetracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_21012011129_assigment_2_expensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
